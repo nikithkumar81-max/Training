8ad396b63f16111b744d85591ae1b0b8111233bc
nikithkumar81-max <nikithkumar81@gmail.com>
2026-03-04 13:15:32 +0530

    first commit

--- day 1/src/program4.java
+++
@@ -1,8 +0,0 @@
-public class program4 {
-    public static void main(String[] args) {
-        int a =131;
-        System.out.println(a);
-        byte b=(byte)a;
-        System.out.println(b);
-    }
-}
